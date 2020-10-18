6f85c3d0a84966b175a55dc07d5b26bc5bda779a
T. Franzel <13507857+tfranzel@users.noreply.github.com>
2020-10-18 15:10:52 +0200

    GIS adaptation

---
+++ tests/contrib/test_rest_framework_gis.py
@@ -0,0 +1,27 @@
+import pytest
+from rest_framework import mixins, serializers, viewsets
+
+try:
+    from rest_framework_gis.fields import GeometryField, GeometrySerializerMethodField
+except ImportError:
+    GeometryField = object
+    GeometrySerializerMethodField = object
+
+from tests import assert_schema, generate_schema
+
+
+class XSerializer(serializers.Serializer):
+    location_method_field = GeometrySerializerMethodField()
+    location_field = GeometryField()
+
+
+class XViewset(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = XSerializer
+
+
+@pytest.mark.contrib('rest_framework_gis')
+def test_rest_framework_gis(no_warnings):
+    assert_schema(
+        generate_schema('x', XViewset),
+        'tests/contrib/test_rest_framework_gis.yml'
+    )
